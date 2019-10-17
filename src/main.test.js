af31a968f996196da7150bfe126839636a17a315
Zach Lysobey <zlysobey@gmail.com>
2019-10-17 01:54:05 -0400

    ✅ add chai assertions

--- src/main.test.js
+++ src/main.test.js
@@ -1,7 +1,7 @@
-const assert = require('assert')
+const { expect } = require('chai')
 
 describe('main.js', function() {
     it('is going to need some tests', function() {
-        assert.equal(1 + 1, 2)
+        expect(1 + 1).to.equal(2)
     })
 })
