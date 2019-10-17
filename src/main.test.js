f5e138a85e9fcb01046a1f8aad5d2b5fce977872
Zach Lysobey <zlysobey@gmail.com>
2019-10-17 01:55:28 -0400

    ✅ add chai-as-promised

--- src/main.test.js
+++ src/main.test.js
@@ -1,7 +1,11 @@
-const { expect } = require('chai')
+const { expect } = require('./test-setup')
 
 describe('main.js', function() {
     it('is going to need some tests', function() {
         expect(1 + 1).to.equal(2)
+    })
+
+    it('will need async tests too', async function() {
+        await expect(Promise.reject('test')).to.be.rejectedWith(/test/)
     })
 })
